1c9249dd7462974f405059e02ff02141bdf0deaf
darkplegug <ammar.rashid221@gmail.com>
2026-02-21 12:54:49 +0700

    fix endpoint login & register

--- frontend/src/api/useRegister.tsx
+++ frontend/src/api/useRegister.tsx
@@ -22,7 +22,7 @@ export default function useRegister() {
       const payload = { username, password }
 
       // PERUBAHAN: Gunakan endpoint '/register' yang ada di server.js kamu
-      const response = await fetch("http://localhost:3000/register", { 
+      const response = await fetch("/api/register", { 
         method: "POST",
         headers: {
           "Content-Type": "application/json",
